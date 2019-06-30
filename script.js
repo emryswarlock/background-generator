8fffac3b2292b7b69158e203e812d9f9b3477220
emryswarlock <ardha275@gmail.com>
2019-06-30 23:48:41 +0700

    add random-color generator

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ var h3 = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2  = document.querySelector(".color2");
 var body = document.querySelector("body");
+var random = document.querySelector("button");
+
+setGradient();
 
 // function to set background-linear-gradient
 function setGradient(){
@@ -15,7 +18,17 @@ function setGradient(){
     h3.innerHTML = body.style.background;
 }
 
+// function random color to generate a random-color
+function randomColor() {
+    color1.value = "#"+((1<<24)*Math.random()|0).toString(16);
+    color2.value = "#"+((1<<24)*Math.random()|0).toString(16);
+    setGradient();
+}
+
 //add an eventListener for color1 & color2 input
 color1.addEventListener("input",setGradient);
 
 color2.addEventListener("input",setGradient);
+
+// add random color button trigger
+random.addEventListener("click",randomColor);
